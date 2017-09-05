bb60352a9ea4b644f51a8e3a12cfa40e08823399
ahmadasjad <ahmadcimage@gmail.com>
2017-09-06 00:49:27 +0530

    Added asset files

---
+++ W3CssAsset.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace ahmadasjad\yii2w3css;
+
+use yii\web\AssetBundle;
+/**
+ * w3.css asset for Yii 2.0 framework
+ *
+ * @author  Ahmad Asjad <ahmadcimage@gmail.com>
+ */
+class W3CssAsset extends AssetBundle
+{
+    public $css = [
+        'css/w3.css',
+    ];
+}
